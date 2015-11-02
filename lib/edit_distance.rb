d95ca857aa19325b863bf7e624230268be770087
dfhoughton <dfhoughton@gmail.com>
2015-11-02 13:32:40 -0500

    proof of concept with Levenshtein algorithm

--- lib/edit_distance.rb
+++ lib/edit_distance.rb
@@ -8,6 +8,7 @@ module EditDistance
         if Proc === proc
           @scale = proc
         else
+          raise "weigh method expected" unless proc.respond_to?(:weigh)
           @scale = -> ( parent, edit, s, d ) { proc.weigh parent, edit, s, d }
         end
       elsif given_block?
@@ -15,7 +16,7 @@ module EditDistance
       else
         raise "no edit weighing algorithm provided"
       end
-      raise "weighing algorithm expected to have arity of 4" unless block.arity == 4
+      raise "weighing algorithm expected to have arity of 4" unless @scale.arity == 4
     end
 
     # returns the edit distance from s1 to s2
@@ -27,10 +28,41 @@ module EditDistance
     def analyze s1, s2
       Matrix.new( s1, s2, @scale ).cell s1.length, s2.length
     end
+
+    def explain s1, s2
+      cell = analyze s1, s2
+      sequence = [ cell ]
+      while !( cell = cell.parent ).root?
+        sequence.unshift cell
+      end
+      sequence.map(&:describe)
+    end
   end
 
   class Cell < Struct.new( :source, :destination, :s, :d, :distance, :parent, :edit )
+
+    def root?
+      parent.nil?
+    end
+    
+    # characters under consideration
+    def chars
+      @chars ||= begin
+        c1 = source[s-1] unless s == 0
+        c2 = destination[d-1] unless d == 0
+        [ c1, c2 ]
+      end
+    end
+
+    # a description of the edit represented
     def describe
+      c1, c2 = chars
+      case edit
+      when :same         then "kept #{c1}"
+      when :insertion    then "inserted #{c2}"
+      when :deletion     then "deleted #{c1}"
+      when :substitution then "substituted #{c2} for #{c1}"
+      end
     end
   end
 
@@ -52,24 +84,24 @@ module EditDistance
         if s == 0
           p = cell s, d - 1
           e = :deletion
-          w = p.distance + @scale.call p, e, s, d
+          w = p.distance + @scale.call( p, e, s, d )
         elsif d == 0
           p = cell s - 1, d
           e = :call
-          w = p.distance + @scale.weigh p, e, s, d
+          w = p.distance + @scale.call( p, e, s, d )
         else
           s1 = s - 1; d1 = d - 1
           c3 = cell s1, d1
           if @source[s1] == @destination[d1]
             p = c3
             w = c3.distance
-            e = Edit::SAME
+            e = :same
           else
             c1 = cell s1, d
             c2 = cell s, d1
-            w1 = c1.distance + @scale.call c1, :deletion, s, d
-            w2 = c2.distance + @scale.call c2, :insertion, s, d
-            w3 = c3.distance + @scale.call c3, :substitution, s, d
+            w1 = c1.distance + @scale.call( c1, :deletion, s, d )
+            w2 = c2.distance + @scale.call( c2, :insertion, s, d )
+            w3 = c3.distance + @scale.call( c3, :substitution, s, d )
             if w1 < w3
               if w1 < w2
                 p = c1
