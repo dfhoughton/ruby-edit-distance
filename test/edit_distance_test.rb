074a8a2756164897b993452bca798666285e5903
dfhoughton <dfhoughton@gmail.com>
2015-11-07 14:36:04 -0500

    constructor tests

--- test/edit_distance_test.rb
+++ test/edit_distance_test.rb
@@ -40,4 +40,27 @@ class EditDistanceTest < Minitest::Test
   def test_lev_cat_dog_edits
     assert_equal edify('substitution substitution substitution'), EditDistance.lev.edits('cat', 'dog')
   end
+
+  def test_lambda_constructor
+    ed = EditDistance::Analyzer.new -> (_,_,_,_) {1}
+    assert_equal 1.0, ed.distance( 'cat', 'cats' )
+  end
+
+  def test_block_constructor
+    ed = EditDistance::Analyzer.new do |_,_,_,_|
+      1
+    end
+    assert_equal 1.0, ed.distance( 'cat', 'cats' )
+  end
+
+  class ED
+    def self.weigh a,b,c,d
+      1
+    end
+  end
+
+  def test_class_constructor
+    ed = EditDistance::Analyzer.new ED
+    assert_equal 1.0, ed.distance( 'cat', 'cats' )
+  end
 end
